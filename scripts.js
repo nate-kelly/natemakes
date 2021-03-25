61c7b2779a4f22cd9980561b299a1e2aca1343a7
ancientmelody <ndavidkelly@gmail.com>
2021-03-24 21:27:07 -0400

    accessibility additions to icons

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-const darkButton = document.querySelector('.dark-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  new SweetScroll({ duration: 800 });
+}, false);
 
+const darkButton = document.querySelector('.dark-toggle');
 darkButton.addEventListener('click', () => {
-	document.body.classList.toggle('dark');
+document.body.classList.toggle('dark');
 });
